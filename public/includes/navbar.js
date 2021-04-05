c49fe35688dcb0cac3ea2b52b0857caf5ad5b339
Radu Tache <radutache76@gmail.com>
2021-04-05 19:35:38 +0300

    More modular

---
+++ public/includes/navbar.js
@@ -0,0 +1,12 @@
+document.getElementById("navbar").innerHTML = `
+      <a href="index.html">
+        <img src="public/img/logo.png" class="logo" >
+      </a>
+      <ul class="nav-bar">
+        <li class="nav-link">
+          <a href="report">Report</a>
+        </li>
+        <li class="nav-link">
+          <a href="about">About</a>
+        </li>
+      </ul>`;
