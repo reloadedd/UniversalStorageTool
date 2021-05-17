f2a0053c962974be855d1e70e70aab034f5a43db
Radu Tache <radutache76@gmail.com>
2021-05-17 20:27:47 +0300

    register and login return a jwt

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 
 exports.create = (req, res) => {
     const User = req.db.users;
@@ -20,24 +21,25 @@ exports.create = (req, res) => {
 
         const user = {
             email: data.email,
-            display_name: data.display_name? data.display_name : data.email,
+            display_name: data.display_name ? data.display_name : data.email,
             password: data.password
         };
 
         User.create(user)
-            .then(data => {
+            .then(async data => {
                 res.writeHead(200, {
                     'Content-Type': 'application/json'
                 });
-                res.end(JSON.stringify(data));
+                res.end(jwt.sign(user, 'shh'));
             })
             .catch(err => {
+                console.log(err);
                 res.writeHead(500, {
                     'Content-Type': 'application/json'
                 });
                 res.end(JSON.stringify({
                     message:
-                        err.errors[0].message || "Some error occurred while creating the Tutorial."
+                        err.errors[0].message || "Some error occurred while creating the User."
                 }));
             });
 
@@ -45,10 +47,42 @@ exports.create = (req, res) => {
 };
 
 exports.findAll = (req, res) => {
-
-};
+}
 
 exports.login = (req, res) => {
+    const User = req.db.users;
+    let data = '';
+    req.on('data', chunk => {
+        data += chunk;
+    })
+    req.on('end', async () => {
+        data = JSON.parse(data);
+        // Validate request
+        if (!data.email || !data.password) {
+            res.writeHead(400, {
+                'Content-Type': 'application/json'
+            });
+            res.end(JSON.stringify({
+                message: 'Content cannot be empty!'
+            }));
+            return;
+        }
+        let thisUser = await User.findOne({where: {email: data.email, password: data.password}})
+        if(!thisUser){
+            res.writeHead(400, {
+                'Content-Type': 'application/json'
+            });
+            res.end(JSON.stringify({
+                message: 'Bad credentials.'
+            }));
+            return;
+        }
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.end(jwt.sign(thisUser.dataValues, 'shh'));
+
+    });
 
 }
 
