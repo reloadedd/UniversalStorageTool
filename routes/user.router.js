e35d5edec2aef6d87cd72e50dcc03419f554298f
Radu Tache <radutache76@gmail.com>
2021-05-21 16:01:57 +0300

    properly invalidate cookie

--- routes/user.router.js
+++ routes/user.router.js
@@ -7,13 +7,13 @@ dispatcher.on('POST', '/register', userController.register);
 dispatcher.on('POST', '/login', userController.login);
 dispatcher.on('POST', '/logout', (req, res) => {
    res.writeHead(200, {
-       'Set-Cookie': 'jwt=; Expires=' + Date.now(),
+       'Set-Cookie': 'jwt=; path=/; httpOnly; Expires=-1',
        'Content-type': 'application/json'
 
-   });
+   })
    res.end(JSON.stringify({
        message: 'logged out'
-   }));
-});
+   }))
+})
 
-module.exports = dispatcher;
+module.exports = dispatcher
