14d98f0d6a2a4a430588716f8d328e91dad378cb
reloadedd <reloadedd@protonmail.com>
2021-05-28 20:23:35 +0300

    Add verification before attempting to change IDs

--- .docker/mariadb_scripts/change_uid.sh
+++ .docker/mariadb_scripts/change_uid.sh
@@ -2,13 +2,23 @@
 
 # ------------
 # Author:       Ionuț Roșca
-# Script:       first_run.sh
-# Description:  This script will prepare the container for the first run (if it is the case)
+# Script:       change_uid.sh
+# Description:  This script will change the UID and GID of a given user.
 # ------------
 
 NEW_UID=997
 NEW_GID=997
 USER_NAME='mysql'
 
-groupmod -g $NEW_GID $USER_NAME
-usermod -u $NEW_UID -g $NEW_GID $USER_NAME
+current_gid=$(id -g $USER_NAME)
+current_uid=$(id -u $USER_NAME)
+
+if [[ "$current_gid" != "$NEW_GID" ]]; then
+  groupmod -g $NEW_GID $USER_NAME
+  echo "Changed GID"
+fi
+
+if [[ "$current_uid" != "$NEW_UID" ]]; then
+  usermod -u $NEW_UID -g $NEW_GID $USER_NAME
+  echo "Changed GID"
+fi
